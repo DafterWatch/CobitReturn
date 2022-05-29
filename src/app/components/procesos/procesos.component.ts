94c9bb25c1e8e3ee57ee8714610e0079b5a366b8
DafterWatch <marcosingefa@gmail.com>
2022-05-29 17:14:04 -0400

    i dont know

--- src/app/components/procesos/procesos.component.ts
+++ src/app/components/procesos/procesos.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { log } from 'console';
 import { FirebaseCobitService } from 'src/app/services/firebase-cobit.service';
 
 @Component({
@@ -9,28 +8,28 @@ import { FirebaseCobitService } from 'src/app/services/firebase-cobit.service';
   styleUrls: ['./procesos.component.scss'],
 })
 export class ProcesosComponent implements OnInit {
-  constructor(private router:Router, private _firebaseCobit:FirebaseCobitService) { }
+  constructor(
+    private router: Router,
+    private _firebaseCobit: FirebaseCobitService
+  ) {}
   procesos: any[] = [];
   procesosLista: any[] = [];
   ngOnInit(): void {
     this.getProcesosFunction();
   }
-  getProcesosFunction(){
-    this._firebaseCobit.getProcesos().subscribe(data => {
+  getProcesosFunction() {
+    this._firebaseCobit.getProcesos().subscribe((data) => {
       this.procesos = [];
-      data.forEach((element:any) => {
+      data.forEach((element: any) => {
         this.procesos.push({
           id: element.payload.doc.id,
-          ...element.payload.doc.data()
-        })
+          ...element.payload.doc.data(),
+        });
       });
       //console.log(this.procesos);
-     //console.log(this.procesos[0].procesos)
-      this.procesosLista = this.procesos[0].procesos["PO1"];
+      //console.log(this.procesos[0].procesos)
+      this.procesosLista = this.procesos[0].procesos['PO1'];
       //console.log(this.procesosLista)
-    })
-  }
-  log(a:any){
-    console.log(a)
+    });
   }
 }
